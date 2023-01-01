5509c71e8ffa7715d0c6f2f43c7265295f768cff
Diego Macias <diego.macias@berkeley.edu>
2022-12-31 17:03:28 -0800

    Created all files and changed authenticate.py to be able to work with other files.

--- authenticate.py
+++ authenticate.py
@@ -1,3 +1,4 @@
+# Import the Spotipy client and the prompt_for_user_token function from the spotipy.util module
 import spotipy
 import spotipy.util as util
 
@@ -8,16 +9,12 @@ CLIENT_SECRET = "6858abef8c7e4b91b938e0af2d7725eb"
 # Replace this with the redirect URI you specified when registering your app
 REDIRECT_URI = "http://localhost:8888/callback"
 
-# Replace this with the username of the user you want to authenticate
-USERNAME = input("Enter your Spotify username: ")
+# Request the username from the user
+username = input("Enter your Spotify username: ")
 
 # Request authorization to access the user's playlists
 scope = "playlist-read-private playlist-read-collaborative"
-token = util.prompt_for_user_token(USERNAME, scope, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI)
+token = util.prompt_for_user_token(username, scope, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI)
 
 # Create a Spotipy client using the authorization token
 sp = spotipy.Spotify(auth=token)
-
-# Test the client by retrieving the user's playlists
-playlists = sp.user_playlists(USERNAME)
-print(playlists)

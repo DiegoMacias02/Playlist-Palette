68ab6143f43f085ea60b0154cd04ca70ba41ba44
Diego Macias <diego.macias@berkeley.edu>
2023-01-10 17:51:13 -0800

    updated song_data to also account for selecting name instead of ID

--- song_data.py
+++ song_data.py
@@ -1,15 +1,19 @@
 from authenticate import sp
-from  select_playlist import selected_id
-# Retrieve the selected playlist
-try:    
-    selected_playlist = sp.playlist(selected_id)
-except: 
-    print("Invalid Playlist Id")
+from select_playlist import selected_name, selected_id
 
-# Extract the names and artists of the songs in the playlist
-songs = []
-for item in selected_playlist["tracks"]["items"]:
-    song = item["track"]
-    name = song["name"]
-    artist = song["artists"][0]["name"]
-    songs.append((name, artist))
+if selected_id:
+    # Retrieve the selected playlist
+    try:    
+        selected_playlist = sp.playlist(selected_id)
+    except: 
+        print(f"Invalid Playlist Id for {selected_name}")
+    
+    # Extract the names and artists of the songs in the playlist
+    songs = []
+    for item in selected_playlist["tracks"]["items"]:
+        song = item["track"]
+        name =song["name"]
+        artist =song["artists"][0]["name"]
+        songs.append((name, artist))
+else:
+    print(f"{selected_name} not found.")

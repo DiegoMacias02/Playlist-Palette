1beab5ff419a1048a487d91cae4b86c4998bd95c
Diego Macias <diego.macias@berkeley.edu>
2023-01-10 17:31:09 -0800

    Updated selecting playlist by name rather than ID

--- select_playlist.py
+++ select_playlist.py
@@ -6,7 +6,19 @@ playlists = sp.user_playlists(username)
 
 # Print the name and ID of each playlist
 for playlist in playlists["items"]:
-    print(f"{playlist['name']} ({playlist['id']})")
+    print(f"{playlist['name']}")
 
 # Prompt the user to select a playlist
-selected_id = input("Enter the ID of the playlist you want to select: ")
+selected_name = input("Enter the name of the playlist you want to select: ")
+selected_id = None
+
+# Loop through the playlists and check if the name entered by the user matches the name of any of the playlists
+for playlist in playlists["items"]:
+    if playlist["name"] == selected_name:
+        selected_id = playlist["id"]
+        break
+
+if selected_id is not None:
+    print(f"Selected playlist: {selected_name, selected_id}")
+else:
+    print("Playlist not found.")

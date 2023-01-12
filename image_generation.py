368ea8bb50dbba8885858b0d50a0bf38a075fcd2
Diego Macias <diego.macias@berkeley.edu>
2023-01-11 17:39:23 -0800

    testing to see if generating a summary for generating the names would result in better names

--- image_generation.py
+++ image_generation.py
@@ -4,7 +4,9 @@ from PIL import Image
 from io import BytesIO
 import os 
 from dotenv import load_dotenv
-from song_data import songs, selected_name
+from song_data import songs
+from name_generation import chosen_name
+
 #run this to get env
 load_dotenv()
 # Replace this with your own API key
@@ -15,9 +17,9 @@ art_style = input("Enter the art style you want to use for your playlist image "
                     + "(e.g. 'watercolor', 'oil', 'graffiti', etc.): ")
 
 # Define a function to generate a summary of the playlist using GPT-3
-def generate_summary(songs, selected_name, art_style):
+def generate_summary(songs, chosen_name, art_style):
     song_string = " ".join([f"{song[0]} by {song[1]}" for song in songs])
-    prompt = f"Summarize the overall vibe, mood and feel of playlist named {selected_name} containing following songs in a few words, also mention the art style you want to use in the summary(this is going to used as a request for dalle-2 so make it as best fit for that): {song_string} {art_style}"
+    prompt = f"Summarize in 2-4 very descriptive and creative sentences, the underlying rhythmic style/groove, mood and feel of playlist named {chosen_name}. {chosen_name} contains the following songs (make sure these 2-4 sentences encapsulates the entire feeling someone would recieve when listening to this playlist): {song_string}"
     completions = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
@@ -32,15 +34,18 @@ def generate_summary(songs, selected_name, art_style):
 # Define a function to generate an image using DALL-E 2 based on the summary
 def generate_image(summary):
     response = openai.Image.create(
-        prompt=f"generate an image based on this summary: {summary}",
+        prompt=f"generate an image based on this summary with this style of imagery {art_style}: {summary}",
         n=1,
         size="1024x1024"
     )
     url = response.data[0]["url"]
     return url
 
+
+summary = generate_summary(songs, chosen_name, art_style)
+print(summary)
 # Get the url of the image
-url = generate_image(generate_summary(songs, selected_name, "abstract"))
+url = generate_image(summary)
 
 # Open the image and show it
 image = Image.open(BytesIO(requests.get(url).content))
--- name_generation.py
+++ name_generation.py
@@ -3,15 +3,41 @@ from song_data import songs
 from dotenv import load_dotenv
 import os
 
+#loading in from my os
 load_dotenv()
 openai.api_key = os.getenv('openai_api_key')
 
-def generate_name(songs, used_names):
+# Define a function to generate a summary of the playlist using GPT-3
+def generate_summary(songs):
     song_string = " ".join([f"{song[0]} by {song[1]}" for song in songs])
-    prompt = f"(Do not include the title (or any words from the tile) of any the songs or any of aritist name in the response) Give me a creative, unique, quirky, and aesthetic name that encapsulates the overall vibe of the following songs, only respond with a 1-5 word name for this playlist and nothing else:\n{song_string}"
+    prompt = f"Summarize in 2-4 very descriptive and creative sentences, the underlying rhythmic style/groove, mood and feel of these songs in a playlist, make sure to NOT USE ANY OF THE SONG NAMES OR ARTIST NAMES' IN THE SUMMARY. This playlist contains the following songs (make sure these 2-4 sentences encapsulates the entire feeling someone would recieve when listening to this playlist): {song_string}"
+    completions = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=128,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    message = completions.choices[0].text
+    return message
+
+#summary of the songs
+summary = generate_summary(songs)
+
+#prompt to feed to gtp-3
+prompt_summary = f"Hello I am working on a project to make playlist-name recommendation to users based on their songs in their playlist."                     
++"I need you to give me one name that you believe fits this playlist and that is it nothing else in the text-response(because I am"
++" using you as an api call currently) you give (Do not include the title, or any words from the tile of any the songs, or any of aritist name in the response)"
++ "Give me a creative, unique, quirky, and aesthetic name that encapsulates the overall vibe of the following songs, only" 
++f"respond with a 1-5 word name for this playlist and nothing else. Here is a summary GTP-3 came up with about the songs: {summary}. " 
++"Please provide me with a 1-5 word response on what you believe is a creative, quirky, and asthetic name that fits this summary."
+
+#generates "playlist-names" based on a summary created by gtp-3 of a user's playlist songs
+def generate_name(summary, used_names):
     response = openai.Completion.create(
         model="text-davinci-002",
-        prompt=prompt,
+        prompt=summary,
         max_tokens=32,
         temperature=0.7,
         top_p=0.9,
@@ -22,7 +48,7 @@ def generate_name(songs, used_names):
     while name in used_names:
         response = openai.Completion.create(
             model="text-curie-001",
-            prompt = f"(Do not include the name of any the songs or any of aritist in the response) Give me a new, creative, unique and aesthetic name that encapsulates the overall vibe of the following songs, only respond with a 1-5 word name for this playlist and nothing else:\n{song_string}",
+            prompt = summary,
             max_tokens=32,
             temperature=0.7,
             top_p=0.9,
@@ -35,7 +61,7 @@ def generate_name(songs, used_names):
 used_names = []
 names = []
 for i in range(5):
-    name = generate_name(songs, used_names)
+    name = generate_name(prompt_summary, used_names)
     names.append(name)
     used_names.append(name)
 
@@ -43,5 +69,5 @@ print("Here are five creative names for your playlist:")
 for count, name in enumerate(names, start=1):
     print(count, name)
 
-selected_name = input("Enter the number of the name you want to select (1-5): ")
-selected_name = names[int(selected_name) - 1]
+chosen_name = input("Enter the number of the name you want to select (1-5): ")
+chosen_name = names[int(chosen_name) - 1]
